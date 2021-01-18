0a2344a00ed9b45d8eea1a88610f6706ff9e41bb
thuglifescientist2018 <thuglifescientist2018@gmail.com>
2021-01-18 09:30:05 +0545

    add basic documentation version 1.0

--- app.js
+++ app.js
@@ -27,44 +27,46 @@ function nameDisplay(name) {
             </em>😂 
             `
 }
-
-   const learnMoreBtn = document.querySelector("#learnMore");
-    const outputbox = document.querySelector("#outputbox");
-   learnMoreBtn.addEventListener("click", learnMoreInfo);
-    function learnMoreInfo() {
-        let i = 0;
-        let element = document.createElement('div');
-        element.setAttribute("class", "bg-white text-dark p-4");
-        element.style.position = "relative";
-        element.style.transition = "1s all";    
-        element.style.top = "-40vh";
-        element.style.border = "2px solid black";
-        element.style.height = "50vh";
-        element.style.width = "50vw";
-        element.style.color = "white";
-   
-        const htmlInfo = fetch('info.html').then(res => res.text()).then(info => {
-            element.innerHTML += info;
-            let close = document.createElement('i');
-            close.style.position = "absolute";
-            close.style.top = "20px";
-            close.style.left = "50%"; 
-            close.style.background = "black";
-            close.style.padding = "10px";
+function learnMore() {
+     const learnMoreBtn = document.querySelector("#learnMore");
+    
+        learnMoreBtn.addEventListener("click", learnMoreInfo);
+        function learnMoreInfo() {
+            
+            let element = document.createElement('div');
+            element.setAttribute("class", "bg-white text-dark p-4");
+            element.style.position = "relative";
+            element.style.transition = "1s all";    
+            element.style.top = "-40vh";
+            element.style.border = "2px solid black";
+            element.style.height = "50vh";
+            element.style.width = "50vw";
+            element.style.color = "white";
         
-            close.style.borderRadius = "50%";
-            close.style.color = "white";
-            close.setAttribute("class", "fas fa-times");
-           element.appendChild(close);
-           close.addEventListener("click", function(){
-               element.remove();
-           })
-        }).catch(console.error('something went wrong'));
-        const lead = document.querySelector(".lead:last-child");
-        element.style.position = "relative";
-        element.style.margin = "0 auto";
-        lead.insertBefore(element, learnMoreBtn);
+            const htmlInfo = fetch('info.html').then(res => res.text()).then(info => {
+                element.innerHTML += info;
+                let close = document.createElement('i');
+                close.style.position = "absolute";
+                close.style.top = "20px";
+                close.style.left = "50%"; 
+                close.style.background = "black";
+                close.style.padding = "10px";
+            
+                close.style.borderRadius = "50%";
+                close.style.color = "white";
+                close.setAttribute("class", "fas fa-times");
+                element.appendChild(close);
+                close.addEventListener("click", function(){
+                    element.remove();
+                })
+            }).catch(console.error('something went wrong'));
+            const lead = document.querySelector(".lead:last-child");
+            element.style.position = "relative";
+            element.style.margin = "0 auto";
+            lead.insertBefore(element, learnMoreBtn);
+            
         
-       
-    }
+     }
+}
 
+learnMore();
